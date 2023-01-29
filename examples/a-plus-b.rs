06c877da1caf425da02428c47ae4299a0a605f03
hayatroid <hayatroid@gmail.com>
2023-01-30 00:36:58 +0900

    verify test

---
+++ examples/a-plus-b.rs
@@ -0,0 +1,11 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
+
+use proconio::input;
+
+fn main() {
+    input! {
+        a: u64,
+        b: u64,
+    }
+    println!("{}", a + b);
+}
