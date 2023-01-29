4f2f40a1a839b7844c1bd259f238d99837958d65
hayatroid <hayatroid@gmail.com>
2023-01-30 00:11:35 +0900

    verify test

--- crates/dp/src/lib.rs
+++ crates/dp/src/lib.rs
@@ -1,13 +1,23 @@
 #[allow(non_snake_case)]
-pub fn knapsack_01<T>(w: Vec<usize>, v: Vec<T>, W: usize) -> T
-where
-    T: Copy + std::ops::Add<Output = T> + std::cmp::Ord + std::default::Default,
-{
-    let mut dp = vec![T::default(); W + 1];
-    for (w, v) in w.into_iter().zip(v) {
+pub fn knapsack_01(w: Vec<usize>, v: Vec<u64>, W: usize) -> u64 {
+    let mut dp = vec![0; W + 1];
+    for (&w, &v) in w.iter().zip(v.iter()) {
         for i in (w..=W).rev() {
             dp[i] = dp[i].max(dp[i - w] + v);
         }
     }
     dp[W]
+}
+
+#[allow(non_snake_case)]
+pub fn knapsack_01_2(w: Vec<u64>, v: Vec<usize>, W: u64) -> usize {
+    let sum = v.iter().sum();
+    let mut dp = vec![W + 1; sum + 1];
+    dp[0] = 0;
+    for (&w, &v) in w.iter().zip(v.iter()) {
+        for i in (v..=sum).rev() {
+            dp[i] = dp[i].min(dp[i - v] + w);
+        }
+    }
+    dp.into_iter().rposition(|x| x <= W).unwrap()
 }
--- examples/knapsack-01-2.rs
+++ examples/knapsack-01-2.rs
@@ -1,16 +1,16 @@
-// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_B
+// verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_1_C
 
-use dp::knapsack_01;
+use dp::knapsack_01_2;
 use proconio::input;
 
 #[allow(non_snake_case)]
 fn main() {
     input! {
         n: usize,
-        W: usize,
-        vw: [(u64, usize); n],
+        W: u64,
+        vw: [(usize, u64); n],
     }
     let w = vw.iter().map(|&p| p.1).collect::<Vec<_>>();
     let v = vw.iter().map(|&p| p.0).collect::<Vec<_>>();
-    println!("{}", knapsack_01(w, v, W));
+    println!("{}", knapsack_01_2(w, v, W));
 }
